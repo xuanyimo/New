d8d2072ba389a7572c8f8a182746ec5c12a2213b
xuanyimo <343557070@qq.com>
2016-01-06 17:33:22 +0800

    some pdf's files and come files

---
+++ python/bs4.py
@@ -0,0 +1,2 @@
+import bs4
+print bs4
---
+++ server/tomcat/HowTomcatWorks/src/ex02/pyrmont/StaticResourceProcessor.java
@@ -0,0 +1,15 @@
+package ex02.pyrmont;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+  public void process(Request request, Response response) {
+    try {
+      response.sendStaticResource();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
